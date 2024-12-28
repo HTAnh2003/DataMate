eacb08ffda687d87b454b16bb9a013683e01e556
Hoang Tien Anh <hoangtienanh2k3@gmail.com>
2024-12-29 00:12:04 +0700

    update run code

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,19 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 import streamlit as st
 
 def execute_plt_code(code: str, df: pd.DataFrame):
     try:
+        code = code.strip('```python\n').strip('```')
         local_vars = {
             "plt": plt,
             "df": df,
+            "sns": sns,
         }
-
+        print("_____code______")
+        print(code)
+        print("___________")
         compiled_code = compile(code, "<string>", "exec")
         exec(compiled_code, globals(), local_vars)
 
